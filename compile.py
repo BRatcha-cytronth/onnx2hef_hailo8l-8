b1012098f626af58b9ef7144b2090d05af0470bb
Ratchadaporn Boonrungsee <boonrungsee.ratcha@cytron.io>
2025-12-15 11:33:28 +0700

    Add files via upload

---
+++ compile.py
@@ -0,0 +1,16 @@
+from hailo_sdk_client import ClientRunner
+
+model_name = 'model'
+quantized_model_har_path = f'/home/user/path/to/{model_name}_quantized_model.har'
+
+runner = ClientRunner(har=quantized_model_har_path)
+
+hef = runner.compile()
+
+file_name = f'{model_name}.hef'
+with open(file_name, 'wb') as f:
+    f.write(hef)
+
+# har_path = f'{model_name}_compiled_model.har'
+# runner.save_har(har_path)
+# !hailo profiler {har_path}j
---
+++ parse.py
@@ -0,0 +1,17 @@
+from hailo_sdk_client import ClientRunner
+
+onnx_model_name = 'model'
+onnx_path = '/home/user/models/model.onnx'
+har_path = '/home/user/models/model.har'
+
+chosen_hw_arch = 'hailo8'
+
+runner = ClientRunner(hw_arch=chosen_hw_arch)
+hn, npz = runner.translate_onnx_model(
+    onnx_path, onnx_model_name,
+    start_node_names=['images'],
+    end_node_names=['/model.22/cv2.0/cv2.0.2/Conv', '/model.22/cv3.0/cv3.0.2/Conv', '/model.22/cv2.1/cv2.1.2/Conv', '/model.22/cv3.1/cv3.1.2/Conv', '/model.22/cv2.2/cv2.2.2/Conv', '/model.22/cv3.2/cv3.2.2/Conv'],
+    net_input_shapes={'images': [1, 3, 640, 640]})
+
+runner.save_har(har_path)
+print(f"Model successfully translated and saved as {har_path}")
